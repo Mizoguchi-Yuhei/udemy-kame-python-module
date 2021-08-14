f8c40a0654dbb04a4c614b560ac0167a12a9ba97
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-14 22:46:45 +0900

    5-59

---
+++ myscript.py
@@ -0,0 +1,8 @@
+import mymodule as mm
+# from mymodule import myfunc, myvariable, anotherfunc
+# from mymodule import *
+# mymodule.myfunc()
+mm.myfunc()
+mm.anotherfunc()
+print(mm.myvariable)
+# print(mymodule.myvariable)
