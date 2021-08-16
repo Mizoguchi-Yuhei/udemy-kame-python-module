d633eee000d62e2f5f90cdec56e84e80a0cfcca1
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-16 21:10:28 +0900

    5-60

--- myscript.py
+++ myscript.py
@@ -1,3 +1,8 @@
+# 標準ライブラリ, サードパーティーのライブラリ, 自分たちのライブラリ, ローカルのライブラリ
+import sys
+# import docstring  # pathにないからエラー
+sys.path.append("/Users/mizoguchiyuutaira/test_programming/udemy_kame_python/basic/function/")
+import docstring
 import mymodule as mm
 # from mymodule import myfunc, myvariable, anotherfunc
 # from mymodule import *
@@ -6,3 +11,5 @@ mm.myfunc()
 mm.anotherfunc()
 print(mm.myvariable)
 # print(mymodule.myvariable)
+print(sys.path)
+print(docstring.multiply(3, 4))
