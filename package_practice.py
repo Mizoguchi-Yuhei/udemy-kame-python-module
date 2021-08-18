47e4a566f61fdcd29092cf7de0ebb696b612cdd2
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-18 23:36:27 +0900

    5-67

--- package_practice.py
+++ package_practice.py
@@ -1,9 +1,19 @@
 # import myfirstpackage.module1
 # import myfirstpackage.module2
-from myfirstpackage import module1, module2
+# from myfirstpackage import module1
+# from myfirstpackage import module2
+
+# import myfirstpackage
+
+from myfirstpackage.subdir import *
 
 # myfirstpackage.module1.myfunc()
 # myfirstpackage.module2.myfunc()
 
-module1.myfunc()
-module2.myfunc()
+# module1.myfunc()
+# module2.myfunc()
+
+# myfirstpackage.myfunc()
+
+myfunc()
+myfunc2()
