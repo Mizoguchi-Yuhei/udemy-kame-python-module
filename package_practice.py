fca9aec1b7c9b33f733585bd930ce56b40faf736
Mizoguchi-Yuhei <mizo.cb.fl@gmail.com>
2021-08-18 23:21:36 +0900

    5-66

---
+++ package_practice.py
@@ -0,0 +1,9 @@
+# import myfirstpackage.module1
+# import myfirstpackage.module2
+from myfirstpackage import module1, module2
+
+# myfirstpackage.module1.myfunc()
+# myfirstpackage.module2.myfunc()
+
+module1.myfunc()
+module2.myfunc()
